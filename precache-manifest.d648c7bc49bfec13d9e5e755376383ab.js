a52d2dc363a0b163f9ee22d99191ec4c6c80475c
root <root@localhost.localdomain>
2018-10-14 18:22:05 +0700

    Updates

---
+++ precache-manifest.d648c7bc49bfec13d9e5e755376383ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "b0df996125ba43715bbb",
+    "url": "/react-app/static/js/main.b0df9961.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-app/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "b0df996125ba43715bbb",
+    "url": "/react-app/static/css/main.dcdb5247.chunk.css"
+  },
+  {
+    "revision": "8a1a4f6a19a213f4021414ef608fd4ad",
+    "url": "/react-app/index.html"
+  }
+];
